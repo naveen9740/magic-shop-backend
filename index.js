89a3760a7e3fdab99785903b7149bcce19c0e03b
NAVEEN KAMATH <naveen.kamath002@gmail.com>
2021-11-30 10:05:12 +0530

    minor bug

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const cors = require("cors");
 
 // initialize app
 const app = express();
